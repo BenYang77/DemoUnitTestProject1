52839b0265a9fb3baef145514aa3067ad732bb21
Bin Ruei Yang <ben_yang77@hotmail.com>
2018-08-16 16:23:29 +0800

    20180816 commit. purpose 嚗?change Batch Script info

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-"# DemoUnitTestProject1" 
-"# DemoUnitTestProject1" 
-"# DemoUnitTestProject3" 
-"# DemoUnitTestProject3" 
-"# DemoUnitTestProject3" 
 ### DemoUnitTestProject 20180816 12:32:51.91 
 # Purpose : test change branch 
 #  Reason : git checkout master  
+ 
+ 
+### DemoUnitTestProject 20180816 16:23:29.23 
+# Purpose : change Batch Script info 
+#  Reason : user do not see how to use it  
  
  
--- bsCreateNewRepo-V3.cmd
+++ bsCreateNewRepo-V3.cmd
@@ -1,21 +1,35 @@
 :: https://help.github.com/articles/adding-an-existing-project-to-github-using-the-command-line/
 @Echo OFF&Cls
-Call :getToday
+
+Set Branch=master
+CHOICE /C 12 /M "Select Branch 1:master, 2:bugfix"
+Set ErrLev=%errorlevel%
+IF %ErrLev%==1 (Set Branch=master&goto DoNextStep)
+IF %ErrLev%==2 (Set Branch=bugfix&goto DoNextStep)
+goto Info00
+
+:DoNextStep
 Set CurProjPath=D:\BenYang\DemoGitByCmd\
+Set originPath=https://github.com/BenYang77/DemoUnitTestProject1.git
+
+Call :getToday
 Call :getCurCmdFilePath %CurProjPath%
 %filedrive%
 ECHO %filedrive%%filepath%
 CD %filepath%
 
-Set originPath=https://github.com/BenYang77/DemoUnitTestProject1.git
 ECHO Update project(DemoUnitTestProject1.git) to %originPath%
+
 SET /P purpose=Please write purpose for it: 
 IF "%purpose%"=="" GOTO Error1
-
+Echo %purpose%
 SET /P Reason=Please write Reason for it: 
 IF "%Reason%"=="" GOTO Error2
 
 ECHO Now it is updating to project! Purpose is %purpose%.
+CHOICE /M "Are you sure updating to the project(%originPath%)"
+Set ErrLev=%errorlevel%
+IF %ErrLev%==2 (GOTO Info01)
 
 echo ### DemoUnitTestProject %today% %time% >> README.md
 echo # Purpose : %purpose% >> README.md
@@ -41,8 +55,9 @@ git commit -m "%today% commit. purpose ： %*"
 :: git remote add origin https://gitforbinrueiyang.visualstudio.com/DemoUnitTestProject/DemoUnitTestProject%20Team/_git/DemoUnitTestProject
 git remote add origin %originPath%
 git remote -v
-git checkout master
-git push -u origin master
+git checkout %Branch%
+:: git push -u origin master
+git push -u origin/%Branch%
 :: git push -u origin bugfix
 EXIT /B 0
 
@@ -80,11 +95,25 @@ FOR %%i IN (%filePathStr%) DO (
 )
 EXIT /B 0
 
+:Info00
+ECHO You have not choiced any branch! Bye bye!!
+pause
+goto End
+
+:Info01
+ECHO You have choiced not to update the project for %Branch%! Bye bye!!
+pause
+goto End
+
 :Error1
 ECHO You did not enter any purpose for it! Bye bye!!
 pause
+goto End
+
 :Error2
 ECHO You did not enter any reason for it! Bye bye!!
 pause
+goto End
+
 :End
 goto :eof
