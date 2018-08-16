883b4cef6919f726186b739da100adbd26ba9381
Bin Ruei Yang <ben_yang77@hotmail.com>
2018-08-16 10:57:29 +0800

    20180816 commit. purpose 嚗?add new batch script

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
-"# DemoUnitTestProject1" 
-"# DemoUnitTestProject1" 
-"# DemoUnitTestProject3" 
-"# DemoUnitTestProject3" 
-"# DemoUnitTestProject3" 
-"# DemoUnitTestProject3" 
+# DemoUnitTestProject  10:31:43.03 
+# Reason yangBinRuei Test 
+ 
+# DemoUnitTestProject  10:32:13.51 
+# Reason update by BenYang 
+ 
+# DemoUnitTestProject 20180816 10:34:40.30 
+# Reason git commit -m --help 
+ 
+# DemoUnitTestProject 20180816 10:36:59.88 
+# Reason yang Bin Ruei 
+ 
+### DemoUnitTestProject 20180816 10:57:28.73 
+# Purpose : add new batch script 
+#  Reason : add new batch script by asked Boss   
+ 
+ 
---
+++ bsCreateNewRepo2.cmd
@@ -0,0 +1,16 @@
+:: https://help.github.com/articles/adding-an-existing-project-to-github-using-the-command-line/
+@Echo OFF&Cls
+echo "# DemoUnitTestProject3" >> README.md
+git init
+:: Under Code Add File
+:: git add README.md
+
+:: Under Code Add Projects
+git add .
+:: git commit -m "first commit"
+:: git commit -m "second commit"
+git commit -m "third commit"
+:: git remote add origin https://github.com/BenYang77/DemoUnitTestProject1.git
+git remote add origin https://gitforbinrueiyang.visualstudio.com/DemoUnitTestProject/DemoUnitTestProject%20Team/_git/DemoUnitTestProject
+git remote -v
+git push -u origin master
